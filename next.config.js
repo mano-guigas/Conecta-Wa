fdc9f99d635f5fe530f7ea74ffeaa413f817b28d
mano-guigas <guii09851@gmail.com>
2026-09-14 06:38:28 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,18 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  typescript: { ignoreBuildErrors: true },
+  eslint: { ignoreDuringBuilds: true },
   async redirects() {
     return [
-      {
-        source: "/",
-        destination: "/login",
-        permanent: false,
-      },
+      { source: "/", destination: "/login", permanent: false },
     ];
   },
 };
