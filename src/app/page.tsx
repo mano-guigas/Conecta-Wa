015a356f1dfcf39d6022587102f1cd9aaf8b2380
mano-guigas <guii09851@gmail.com>
2026-09-14 07:59:54 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,17 @@
-import { redirect } from "next/navigation";
+'use client'
+import { useEffect } from 'react'
 
-export default function HomePage() {
-  redirect("/login");
+export default function Home() {
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker.getRegistrations().then(regs => {
+        regs.forEach(r => r.unregister())
+      })
+      if ('caches' in window) {
+        caches.keys().then(keys => keys.forEach(k => caches.delete(k)))
+      }
+    }
+    window.location.replace('/login')
+  }, [])
+  return <p style={{color:'white', padding: '20px'}}>Limpando e redirecionando...</p>
 }
